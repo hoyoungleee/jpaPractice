3300cc11abd757c0833f93f965b8bd28d74b6f02
hoyoungleee <ghdud939@gmail.com>
2025-04-18 17:01:21 +0900

    JPA실습프로젝트 영속성 테스트 구현

---
+++ src/main/java/com/study/jpa/chap01/entity/Product.java
@@ -0,0 +1,47 @@
+package com.study.jpa.chap01.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id") // id 필드가 같으면 같은 객체로 처리하겠다.
+@Entity // 이 클래스는 JPA가 관리한다. 이 클래스는 데이터베이스의 한 행에 정확히 대응한다.
+@Table(name="tbl_product")
+public class Product {
+
+    @Id //pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// auto_increment
+    @Column(name = "prod_id")
+    private long id;
+
+    @Column(name="prod_name", length = 30, nullable = false)
+    private String name;
+
+    @Column(name="prod_price")
+    private int price;
+
+    @Column(nullable = false) // not null
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @CreationTimestamp // insert 시에 자동으로 서버 시간 저장
+    @Column(updatable = false) //수정 불가
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp // update 실행시 자동으로 시간이 저장
+    private LocalDateTime updatedAt;
+
+    // 데이터베이스에는 저장 안하고 클래스 내부에서만 사용할 필드
+    @Transient
+    private String nickName;
+
+    public enum Category {
+        FOOD, FASHION, ELECTRONIC
+    }
+}
--- src/test/java/com/study/jpa/JpaStudyApplicationTests.java
+++ src/test/java/com/study/jpa/JpaStudyApplicationTests.java
@@ -1,13 +1,63 @@
 package com.study.jpa;
 
+import com.study.jpa.chap01.entity.Product;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional // 테스트 이후 rollback이 진행되면서 테스트 이전의 DB 환경으로 돌아갑니다.
+@Rollback(false) // 롤백 방지 -> 테스트 결과가 DB에 그대로 반영.
 class JpaStudyApplicationTests {
 
+    @Autowired
+    EntityManager em; //JPA에서 엔터티를 다룰 수 있게 해주는 객체.
+
+
     @Test
-    void contextLoads() {
+    @DisplayName("상품을 데이터베이스에 저장한다.") //테스트 목표
+    void saveTest() {
+        // given: 준비 -> 테스트에 사용할 변수, 입력값 등을 정의하는 곳.
+        Product product = new Product();
+        product.setName("신발");
+        product.setPrice(90000);
+        product.setCategory(Product.Category.FASHION);
+        // when: 실행 -> 테스트를 실행하는 메인 로직
+        em.persist(product);
+
+        // then: 검증 -> 예상한 값, 실제 실행한 값을 확인하는 부분.
     }
 
+    @Test
+    @DisplayName("아이디에 맞는 상품가져오기")
+    void findByIdTest() {
+        // given: 준비 -> 테스트에 사용할 변수, 입력값 등을 정의하는 곳.
+        Long id = 1L;
+        // when: 실행 -> 테스트를 실행하는 메인 로직
+        Product product = em.find(Product.class, id);
+        // then: 검증 -> 예상한 값, 실제 실행한 값을 확인하는 부분.
+        assertEquals(product.getName(),"신발");
+    }
+
+    @Test
+    @DisplayName("영속성 컨텍스트의 1차 캐시 사용")
+    void persistTest() {
+        // given: 준비 -> 테스트에 사용할 변수, 입력값 등을 정의하는 곳.
+        Product product = new Product();
+        product.setName("짜장면");
+        product.setPrice(8000);
+        product.setCategory(Product.Category.FOOD);
+        // when: 실행 -> 테스트를 실행하는 메인 로직
+        em.persist(product);
+        Product foundProd = em.find(Product.class, 2L);
+        // then: 검증 -> 예상한 값, 실제 실행한 값을 확인하는 부분.
+        assertEquals(8000, foundProd.getPrice());
+    }
 }
