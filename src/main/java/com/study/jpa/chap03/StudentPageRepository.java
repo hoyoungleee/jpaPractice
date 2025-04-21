7befbccdd8a7c0d2f94e9048da8ab79f5d70ee13
hoyoungleee <ghdud939@gmail.com>
2025-04-21 12:47:20 +0900

    JPA 페이지

---
+++ src/main/java/com/study/jpa/chap03/StudentPageRepository.java
@@ -0,0 +1,14 @@
+package com.study.jpa.chap03;
+
+import com.study.jpa.chap02.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentPageRepository extends JpaRepository<Student, String> {
+
+    // 학생의 이름에 특정 단어가 포함된 걸 조회 + 페이징 정보
+    // 커스텀 메서드를 만들 때 페이지 정보를 얻고 싶으면 Pageable을 매개값으로 받으세요.
+    Page<Student> findByNameContaining(String name, Pageable pageable);
+
+}
---
+++ src/test/java/com/study/jpa/chap03/StudentPageRepositoryTest.java
@@ -0,0 +1,111 @@
+package com.study.jpa.chap03;
+
+import com.study.jpa.chap02.entity.Student;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class StudentPageRepositoryTest {
+
+    @Autowired
+    private StudentPageRepository studentPageRepository;
+
+    @BeforeEach
+    void setUp() {
+        for (int i = 1; i <= 147; i++) {
+            Student s = Student.builder()
+                    .name("김메롱" + i)
+                    .city("무슨무슨시"+ i)
+                    .major("어쩔전공"+i)
+                    .build();
+            studentPageRepository.save(s);
+        }
+    }
+
+    @Test
+    @DisplayName("기본적인 페이지 조회 테스트")
+    void basicPageTest() {
+        // given: 준비 -> 테스트에 사용할 변수, 입력값 등을 정의하는 곳.
+        int pageNo = 6;// 사용자가 머무는 페이지 번호
+        int amount = 10; // 한페이지에 보여줄 양
+        // 페이지 정보 객체 생성 (Pageable)
+        // 여기서는 페이지 번호가 zero-based임: 1페이지를 0으로 취급
+        Pageable pageable = PageRequest.of(pageNo -1, amount);
+        // when: 실행 -> 테스트를 실행하는 메인 로직
+        Page<Student> students = studentPageRepository.findAll(pageable);
+
+        // 실질적 데이터 꺼내기
+        List<Student> studentList = students.getContent();
+
+        // 총 페이지 수
+        int totalPages = students.getTotalPages();
+
+        // 총 학생 수
+        long totalElements = students.getTotalElements();
+
+        // 현재 페이지 기준 이전, 다음페이지 존재여부
+        boolean next = students.hasNext();
+        boolean prev = students.hasPrevious();
+
+
+        // then: 검증 -> 예상한 값, 실제 실행한 값을 확인하는 부분.
+        System.out.println("\n\n\n");
+        System.out.println("prev = " + prev);
+        System.out.println("next = " + next);
+        System.out.println("totalPages = " + totalPages);
+        System.out.println("totalElements = " + totalElements);
+        System.out.println("studentList = " + studentList);
+        System.out.println("\n\n\n");
+    }
+
+    /*
+     페이징 처리 시에 버튼 알고리즘은 jpa에서 따로 제공하지 않기 때문에
+     버튼 배치 알고리즘을 수행할 클래스는 여전히 필요합니다.
+     제공되는 정보는 이전보다 많기 때문에, 좀 더 수월하게 처리가 가능합니다.
+     */
+    @Test
+    @DisplayName("이름검색 + 페이징")
+    void testSearchAndPaging() {
+        // given: 준비 -> 테스트에 사용할 변수, 입력값 등을 정의하는 곳.
+        int pageNo = 3;
+        int amount = 6;
+        Pageable pageable = PageRequest.of(pageNo -1, amount);
+
+        // when: 실행 -> 테스트를 실행하는 메인 로직
+        Page<Student> students = studentPageRepository.findByNameContaining("3",pageable);
+
+        // 실질적 데이터 꺼내기
+        List<Student> studentList = students.getContent();
+
+        // 총 페이지 수
+        int totalPages = students.getTotalPages();
+
+        // 총 학생 수
+        long totalElements = students.getTotalElements();
+
+        // 현재 페이지 기준 이전, 다음페이지 존재여부
+        boolean next = students.hasNext();
+        boolean prev = students.hasPrevious();
+
+        // then: 검증 -> 예상한 값, 실제 실행한 값을 확인하는 부분.
+        System.out.println("\n\n\n");
+        System.out.println("prev = " + prev);
+        System.out.println("next = " + next);
+        System.out.println("totalPages = " + totalPages);
+        System.out.println("totalElements = " + totalElements);
+        System.out.println("studentList = " + studentList);
+        System.out.println("\n\n\n");
+    }
+}
